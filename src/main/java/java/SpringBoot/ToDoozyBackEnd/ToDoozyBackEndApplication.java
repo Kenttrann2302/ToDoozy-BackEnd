c46018d78c3d108daf656142f38ff017ea2cfda9
Kenttrann2302 <110959350+Kenttrann2302@users.noreply.github.com>
2023-04-30 23:56:51 -0400

    Initial commit

---
+++ src/main/java/java/SpringBoot/ToDoozyBackEnd/ToDoozyBackEndApplication.java
@@ -0,0 +1,13 @@
+package java.SpringBoot.ToDoozyBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoozyBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoozyBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/java/SpringBoot/ToDoozyBackEnd/ToDoozyBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package java.SpringBoot.ToDoozyBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoozyBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
